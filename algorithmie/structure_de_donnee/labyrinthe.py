18adbeb5da07749c322ff32d11b64cbbd6e11dc4
Atellane <anonymousconcept1@gmail.com>
2024-02-07 20:48:07 +0100

    importation de random pour aléatoirisez les coordonnées de la sortie du labyrinthe, fonction main_a_droite terminée, optimisation de l'affichage du parcours des algorithmes, ajout de la représentation graphique de l'entrée (bleu) et de la sortie (rouge)

--- algorithmie/structure_de_donnee/labyrinthe.py
+++ algorithmie/structure_de_donnee/labyrinthe.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 from graphe import Graphe
 from time import sleep
+from random import randint
 
 class Labyrinthe:
     """Implémentation d'une classe permettant la génération facile et rapide de labyrinthe avec tkinter."""
-    def __init__(self: object, largeur: int, hauteur: int) -> object:
+    def __init__(self: object, hauteur: int, largeur: int) -> object:
         self.largeur = largeur
         self.hauteur = hauteur
         self.__grille: list = self.__generer_grille()
@@ -36,10 +37,12 @@ class Labyrinthe:
                 self.__canvas.create_line(x, y, x, y + 20, width=2)
                 self.__canvas.create_line(x + 20, y, x + 20, y + 20, width=2)
 
-        bouton_effacer: Button = tk.Button(self.__racine, text="Effacer le chemin suivi", command=self.suppr_chemin_suivi)
-        bouton_effacer.pack(side=tk.BOTTOM, pady=10)
+        bouton_effacer: Button = tk.Button(self.__racine, text="Effacer le chemin suivi.", command=self.suppr_chemin_suivi)
+        bouton_effacer.pack(side=tk.BOTTOM)
         bouton_dijsktra: Button = tk.Button(self.__racine, text="dijkstra", command=self.dijsktra_graphe)
-        bouton_dijsktra.pack(side=tk.BOTTOM, pady=10)
+        bouton_dijsktra.pack(side=tk.BOTTOM)
+        bouton_main_a_droite: Button = tk.Button(self.__racine, text="main à droite", command=self.main_a_droite)
+        bouton_main_a_droite.pack(side=tk.BOTTOM)
 
     def creer_graphe_a_partir_labyrinthe(self: object) -> Graphe:
         """Crée un graphe à partir de l'objet Labyrinthe."""
@@ -65,58 +68,87 @@ class Labyrinthe:
 
         return graph
 
-    def main_a_droite_graphe(self: object, entree: tuple=None, sortie: tuple=None) -> None:
+    def main_a_droite(self: object, entree: tuple=None, sortie: tuple=None) -> None:
         """Implémentation de la résolution de labyrinthe par la technique de la main droite."""
+        self.suppr_chemin_suivi()
         if entree == None:
             entree = (0, 0)
         if sortie == None:
-            sortie = (self.hauteur - 1, self.largeur - 1)
+            sortie = (self.hauteur - randint(1, self.hauteur-2), self.largeur - randint(1, self.largeur-2))
+        self.__canvas.create_rectangle(entree[1]*20, entree[0]*20, entree[1]*20 + 20, entree[0]*20 + 20, fill="blue", outline="", tags="entree")
+        self.__canvas.create_rectangle(sortie[1]*20, sortie[0]*20, sortie[1]*20 + 20, sortie[0]*20 + 20, fill="red", outline="", tags="sortie")
+        self.__racine.update_idletasks()
+        sleep(1)
         chemin_suivi: list = [entree]
         sommet_courant: tuple = entree
 
         # Affichez le chemin suivi après chaque itération avec une pause
-        self.afficher_chemin_suivi_direct(chemin_suivi)
+        self.__canvas.create_rectangle(sommet_courant[1]*20, sommet_courant[0]*20, sommet_courant[1]*20 + 20, sommet_courant[0]*20 + 20, fill="green", outline="", tags="chemin_suivi")
         self.__racine.update_idletasks()
-        sleep(0.027)  # Pause de 0.027 seconde (ajustez selon vos besoins)
+        # sleep(0.027)  # Pause de 0.027 seconde (ajustez selon vos besoins)
 
         while sommet_courant != sortie:
-            pass
+            if (sommet_courant[0], sommet_courant[1] + 1) in self.__graph.voisins(sommet_courant) and not ((sommet_courant[0], sommet_courant[1] + 1) in chemin_suivi):
+                sommet_courant = (sommet_courant[0], sommet_courant[1] + 1)
+                chemin_suivi.append(sommet_courant)
+                self.__canvas.create_rectangle(sommet_courant[1]*20, sommet_courant[0]*20, sommet_courant[1]*20 + 20, sommet_courant[0]*20 + 20, fill="green", outline="", tags="chemin_suivi")
+                self.__racine.update_idletasks()
+                # sleep(0.027)
+            else:
+                if (sommet_courant[0] + 1, sommet_courant[1]) in self.__graph.voisins(sommet_courant) and not ((sommet_courant[0] + 1, sommet_courant[1]) in chemin_suivi):
+                    sommet_courant = (sommet_courant[0] + 1, sommet_courant[1])
+                    chemin_suivi.append(sommet_courant)
+                    self.__canvas.create_rectangle(sommet_courant[1]*20, sommet_courant[0]*20, sommet_courant[1]*20 + 20, sommet_courant[0]*20 + 20, fill="green", outline="", tags="chemin_suivi")
+                    self.__racine.update_idletasks()
+                    # sleep(0.027)
+                else:
+                    if (sommet_courant[0], sommet_courant[1] - 1) in self.__graph.voisins(sommet_courant) and not ((sommet_courant[0], sommet_courant[1] - 1) in chemin_suivi):
+                        sommet_courant = (sommet_courant[0], sommet_courant[1] - 1)
+                        chemin_suivi.append(sommet_courant)
+                        self.__canvas.create_rectangle(sommet_courant[1]*20, sommet_courant[0]*20, sommet_courant[1]*20 + 20, sommet_courant[0]*20 + 20, fill="green", outline="", tags="chemin_suivi")
+                        self.__racine.update_idletasks()
+                        # sleep(0.027)
+                    else:
+                        if (sommet_courant[0] - 1, sommet_courant[1]) in self.__graph.voisins(sommet_courant) and not((sommet_courant[0] - 1, sommet_courant[1]) in chemin_suivi):
+                            sommet_courant = (sommet_courant[0] - 1, sommet_courant[1])
+                            chemin_suivi.append(sommet_courant)
+                            self.__canvas.create_rectangle(sommet_courant[1]*20, sommet_courant[0]*20, sommet_courant[1]*20 + 20, sommet_courant[0]*20 + 20, fill="green", outline="", tags="chemin_suivi")
+                            self.__racine.update_idletasks()
+
 
 
     def dijsktra_graphe(self: object, entree: tuple=None, sortie: tuple=None) -> None:
         """Implémentation de la résolution de labyrithe à l'aide de l'algorithme de dijsktra."""
+        self.suppr_chemin_suivi()
         if entree == None:
             entree = (0, 0)
         if sortie == None:
-            sortie = (self.hauteur - 1, self.largeur - 1)
+            sortie = (self.hauteur - randint(1, self.hauteur-2), self.largeur - randint(1, self.largeur-2))
+        self.__canvas.create_rectangle(entree[1]*20, entree[0]*20, entree[1]*20 + 20, entree[0]*20 + 20, fill="blue", outline="", tags="entree")
+        self.__canvas.create_rectangle(sortie[1]*20, sortie[0]*20, sortie[1]*20 + 20, sortie[0]*20 + 20, fill="red", outline="", tags="sortie")
+        self.__racine.update_idletasks()
         distances = self.__graph.dijkstra(sortie)
         chemin_suivi: list = [entree]
         sommet_courant: tuple = entree
 
         # Affichez le chemin suivi après chaque itération avec une pause
-        self.afficher_chemin_suivi_direct(chemin_suivi)
+        self.__canvas.create_rectangle(sommet_courant[1]*20, sommet_courant[0]*20, sommet_courant[1]*20 + 20, sommet_courant[0]*20 + 20, fill="green", outline="", tags="chemin_suivi")
         self.__racine.update_idletasks()
-        sleep(0.027)  # Pause de 0.027 seconde (ajustez selon vos besoins)
+        # sleep(0.027)  # Pause de 0.027 seconde (ajustez selon vos besoins)
 
-        while distances[sommet_courant][1] is not None:
+        while distances[sommet_courant][1] != None:
             sommet_courant = distances[sommet_courant][1]
             chemin_suivi.append(sommet_courant)
-            self.afficher_chemin_suivi_direct(chemin_suivi)
+            self.__canvas.create_rectangle(sommet_courant[1]*20, sommet_courant[0]*20, sommet_courant[1]*20 + 20, sommet_courant[0]*20 + 20, fill="green", outline="", tags="chemin_suivi")
             self.__racine.update_idletasks()
-            sleep(0.027)
-
-    def afficher_chemin_suivi_direct(self: object, chemin_suivi: list) -> None:
-        """Affiche le chemin parcouru."""
-        if self.__racine.winfo_exists():
-            self.__canvas.delete("chemin_suivi")
-            for i, j in chemin_suivi:
-                x, y = j*20, i*20
-                self.__canvas.create_rectangle(x, y, x + 20, y + 20, fill="green", outline="", tags="chemin_suivi")
+            # sleep(0.027)
 
     def suppr_chemin_suivi(self: object) -> None:
         """Supprime la représentation du chemin parcouru."""
         if self.__racine.winfo_exists():
             self.__canvas.delete("chemin_suivi")
+            self.__canvas.delete("sortie")
+            self.__canvas.delete("entree")
 
 
     def mainloop(self: object) -> None:
@@ -125,6 +157,6 @@ class Labyrinthe:
 
 # Exemple d'utilisation
 if __name__ == "__main__":
-    labyrinthe = Labyrinthe(70, 45)
+    labyrinthe = Labyrinthe(40, 94)
     labyrinthe.afficher()
     labyrinthe.mainloop()
