e6fa579779935fcbf58b69abc66614a1e2ccf8e0
Atellane <anonymousconcept1@gmail.com>
2024-03-25 13:43:58 +0100

    ajout premier fichiers projet jeux et exemple pygame

--- algorithmie/projet_jeu_video/jeux.py
+++ algorithmie/projet_jeu_video/jeux.py
@@ -14,11 +14,11 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    # ballrect = ballrect.move(speed)
-    # if ballrect.left < 0 or ballrect.right > width:
-    #     speed[0] = -speed[0]
-    # if ballrect.top < 0 or ballrect.bottom > height:
-    #     speed[1] = -speed[1]
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
 
     screen.fill(black)
     screen.blit(ball, ballrect)
